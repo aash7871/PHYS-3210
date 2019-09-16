2144c02253ef9b55dc0dd9fa2187fde25c3696fd
Amanda <amanda.lee.ash@gmail.com>
2019-09-16 19:24:19 -0400

    E8_writeup is getting there

--- Week 05/E8.py
+++ Week 05/E8.py
@@ -26,16 +26,19 @@ plt.plot(x[1:], f_prime[1:], '.', label = 'forward difference', alpha = 0.2, col
 plt.plot(x, f_prime_numpy, '.', label = 'numpy', alpha = 0.2, color = 'r')
 plt.plot(x, np.cos(x), '.', label = 'exact', alpha = 0.2, color = 'g')
 plt.legend()
+plt.savefig('first_derivative.pdf')
 plt.show()
 
-error_forward = np.cos(x)[1:] - f_prime[1:]
+error_forward = (np.cos(x)[1:] - f_prime[1:])/np.cos(x)[1:]
 plt.title('first derivative error forward difference')
 plt.plot(x[1:], error_forward, '.')
+plt.savefig('forward_diff_err.pdf')
 plt.show()
 
-error_numpy = np.cos(x) - f_prime_numpy
+error_numpy = (np.cos(x) - f_prime_numpy)/(np.cos(x))
 plt.title('first derivative error numpy')
 plt.plot(x[:-1], error_numpy[:-1], '.')
+plt.savefig('grad_err.pdf')
 plt.show()
 
 #second derivatives 
@@ -52,16 +55,19 @@ plt.plot(x[2:], f_prime_prime[2:], '.', label = 'forward difference', alpha = 0.
 plt.plot(x, f_prime_prime_numpy, '.', label = 'numpy', alpha = 0.2, color = 'r')
 plt.plot(x, -np.sin(x), '.', label = 'exact', alpha = 0.2, color = 'g')
 plt.legend()
+plt.savefig('2nd_derivative.pdf')
 plt.show()
 
-error_forward_2 = -np.sin(x)[2:] - f_prime_prime[2:]
+error_forward_2 = (-np.sin(x)[2:] - f_prime_prime[2:])/(-np.sin(x)[2:])
 plt.title('second derivative error forward difference')
 plt.plot(x[2:], error_forward_2, '.')
+plt.savefig('2nd_derivative_err_forward_diff.pdf')
 plt.show()
 
-error_numpy_2 = -np.sin(x) - f_prime_prime_numpy
+error_numpy_2 = (-np.sin(x) - f_prime_prime_numpy)/-np.sin(x)
 plt.title('second derivative error numpy')
 plt.plot(x[1:-2], error_numpy_2[1:-2], '.')
+plt.savefig('2nd_derivative_err_numpy.pdf')
 plt.show()
 
 #random noise
@@ -83,6 +89,7 @@ plt.plot(x[1:], f_prime[1:], '.', label = 'forward difference', alpha = 0.2, col
 plt.plot(x, f_prime_numpy, '.', label = 'numpy', alpha = 0.2, color = 'r')
 plt.plot(x, np.cos(x), '.', label = 'exact', alpha = 0.2, color = 'g')
 plt.legend()
+plt.savefig('noisy_derivative.pdf')
 plt.show()
 
 error_forward = np.cos(x)[1:] - f_prime[1:]
